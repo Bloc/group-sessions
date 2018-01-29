ead8e3b61ae926f0d06875075886a06f69693f00
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2018-01-29 06:59:05 -0800

    adds more slides and cleans up example.rb

--- content/decks/ruby-debugging/ruby/example.rb
+++ content/decks/ruby-debugging/ruby/example.rb
@@ -5,12 +5,11 @@ def powersOfTwo(n)
   (0..n).to_a.each_with_index do |x, i|
     # set a print to see what's happening
     # p i
-    debugger
     newArr.push(2*i)
   end
 
   newArr
 end
 
-p powersOfTwo(2)
+puts powersOfTwo(2)
 # [1,2,4]
