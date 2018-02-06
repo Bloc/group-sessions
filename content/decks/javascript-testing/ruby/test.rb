8eef8ac01cc5dc01573e9fcb62175138971e056d
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2018-02-06 07:05:12 -0800

    adds javascript testing

---
+++ content/decks/javascript-testing/ruby/test.rb
@@ -0,0 +1,5 @@
+describe "Solution" do
+  it "should test for something" do
+    Test.assert_equals("actual", "expected", "This is just an example of how you can write your own TDD tests")
+  end
+end
