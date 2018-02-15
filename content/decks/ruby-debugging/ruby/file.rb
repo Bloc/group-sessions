f62be2fe3eed8353869603ee0bb8859429b0b6e8
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2018-02-15 06:06:17 -0800

    fix ruby debugging

--- content/decks/ruby-debugging/ruby/file.rb
+++ content/decks/ruby-debugging/ruby/file.rb
@@ -1,16 +1,14 @@
-begin
-  File.open('exception.rb', 'r') do |f1|
-    while line = f1.gets
-      puts line
-    end
-  end
-
-  # Create a new file and write to it
-  File.open('exception.rb', 'w') do |f2|
-    # use "" for two lines of text
-    f2.puts "Created by bdougieYO"
-  end
-rescue Exception => msg
-  # display the system generated error message
-  puts msg
+def do_some_math(n)
+  1/n
 end
+
+def print_something
+  p "something"
+end
+
+def run_program
+  do_some_math(4)
+  print_something
+end
+
+run_program
