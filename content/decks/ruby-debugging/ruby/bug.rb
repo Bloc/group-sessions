7d580d8f37e94d395a4ca64e63b3acf560066c36
Brian Douglas <bdougie@Brians-MacBook-Pro.local>
2018-02-20 11:44:00 -0800

    make ruby methods ruby like

--- content/decks/ruby-debugging/ruby/bug.rb
+++ content/decks/ruby-debugging/ruby/bug.rb
@@ -1,14 +1,14 @@
 # the problem is return the wrong answer, set a breakpoint to find out what is wrong
 
 def powers_of_two(n)
-  newArr = []
+  new_arr = []
   (0..n).to_a.each_with_index do |x, i|
     # set a print to see what's happening
     # p i
-    newArr.push(2*i)
+    new_arr.push(2*i)
   end
 
-  newArr
+  new_arr
 end
 
 puts powers_of_two(2)
